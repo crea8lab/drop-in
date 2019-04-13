69c83cf7d23e3920258954c3f55f346f7326b1d9
Fayvor George <phavorsparks@gmail.com>
2019-04-13 21:28:36 +0100

    Output team members in the Team Page

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,11 +1,47 @@
-<template>
+x<template>
   <div class="team">
     <h1 class="subheading grey--text">Team</h1>
 
     <!-- Hold contents in place inside a container -->
     <v-container class="my-5">
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet aliquam corrupti illo quo quaerat accusamus earum doloremque! Voluptatem inventore adipisci praesentium unde incidunt illo sequi obcaecati, tempora similique voluptatum harum.</p>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet aliquam corrupti illo quo quaerat accusamus earum doloremque! Voluptatem inventore adipisci praesentium unde incidunt illo sequi obcaecati, tempora similique voluptatum harum.</p>
+      <v-layout row wrap>
+        <v-flex xs12 sm6 md4 lg3 v-for="(person, i) in team" :key="i">
+          <v-card flat class="text-xs-center ma-3">
+            <v-responsive class="pt-4">
+              <v-avatar size="100" class="grey lighten-2">
+                <v-img :src="`/images/${person.avatar}`"/>
+              </v-avatar>
+            </v-responsive>
+            <v-card-text>
+              <div class="subheading">{{ person.name }}</div>
+              <div class="grey--text">{{ person.role }}</div>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-btn flat color="grey">
+                <v-icon small left>message</v-icon>
+                <span>Message</span>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
     </v-container>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      team: [
+        { name: "Code Slow", role: "Web Developer", avatar: "/avatar-1.png" },
+        { name: "Sy Ryu", role: "Graphic Designer", avatar: "/avatar-2.png" },
+        { name: "Yoshi", role: "Product Designer", avatar: "/avatar-3.png" },
+        { name: "Evan Yu", role: "Media Strategist", avatar: "/avatar-4.png" },
+        { name: "Gowen ku", role: "Digital Marketer", avatar: "/avatar-5.png" }
+      ]
+    };
+  }
+};
+</script>
