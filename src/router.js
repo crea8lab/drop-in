d2aeb4d0757ced9733ab6f5564190a86effd1a93
Fayvor George <phavorsparks@gmail.com>
2019-04-13 12:50:25 +0100

    Edit the router

--- src/router.js
+++ src/router.js
@@ -1,21 +1,29 @@
-import Vue from "vue";
-import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Vue from 'vue';
+import Router from 'vue-router';
+import Dashboard from './views/Dashboard.vue';
+import Project from './views/Project.vue';
+import Team from './views/Team.vue';
 
 Vue.use(Router);
 
 export default new Router({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: Home
-    },
-    {
-      path: "/about",
-      name: "about"
-    }
-  ]
+	mode: 'history',
+	base: process.env.BASE_URL,
+	routes: [
+		{
+			path: '/',
+			name: 'dashboard',
+			component: Dashboard
+		},
+		{
+			path: '/projects',
+			name: 'project',
+			component: Project
+		},
+		{
+			path: '/team',
+			name: 'team',
+			component: Team
+		}
+	]
 });
